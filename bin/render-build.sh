2773e1058b009adbdf635503c7880799417dfa21
ytimesy <yukio.juna1224@gmail.com>
2023-01-30 16:19:39 +0900

    デプロイエラー対応

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,22 @@
 # exit on error
 set -o errexit
 
+# Download ChromeDriver
+wget https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip
+
+# Unzip ChromeDriver
+unzip chromedriver_linux64.zip
+
+# Make ChromeDriver executable
+chmod +x chromedriver
+
+# Move ChromeDriver to the current directory
+APP_DIR="./"
+mv chromedriver $APP_DIR
+
+# Set the path to ChromeDriver in your application
+export PATH=$PATH:$APP_DIR
+
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
