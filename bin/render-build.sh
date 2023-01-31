a9ef6053417afbb5776faa2666f158f51dbfda18
ytimesy <yukio.juna1224@gmail.com>
2023-01-31 12:38:41 +0900

    デプロイエラー対応

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,6 @@
 # exit on error
 set -o errexit
 
-# install chromedriver Possible alternatives gem
-gem install chromedriver-installer
-gem install chromedriver-helper
-gem install capybara-chromedriver-logger
-
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
