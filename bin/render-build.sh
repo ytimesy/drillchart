72c6caeb36bc73ff75de36f5dd965cf2fede4fa2
ytimesy <yukio.juna1224@gmail.com>
2023-01-31 11:39:57 +0900

    デプロイエラー対応

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,23 +2,9 @@
 # exit on error
 set -o errexit
 
-# Download ChromeDriver
-wget https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip
+# install chromedriver gem
+gem install chromedriver
 
-# Unzip ChromeDriver
-unzip chromedriver_linux64.zip
-
-# Make ChromeDriver executable
-chmod +x chromedriver
-
-# ChromeDriver to the current directory
-APP_DIR="./"
-
-# Set the path to ChromeDriver in my application
-export PATH=$PATH:$APP_DIR
-
-# install webdriver gem
-gem install selenium-webdriver
 
 bundle install
 bundle exec rake assets:precompile
