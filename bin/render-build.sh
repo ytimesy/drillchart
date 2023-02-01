b2a2cc7e01ae3f2d436ac455f2b23a5b06fea24f
ytimesy <yukio.juna1224@gmail.com>
2023-02-01 10:37:30 +0900

    デプロイエラー対応（多分これでもだめ）

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,12 +1,9 @@
+#!/bin/bash
+export PATH=$PATH:'./'
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-# Set the path to ChromeDriver in my application
-export CHROME_DRIVER_BIN=`which chromedriver`
-export PATH=$PATH:$CHROME_DRIVER_BIN
-export PATH=$PATH:'./'
-
 
 bundle install
 bundle exec rake assets:precompile
