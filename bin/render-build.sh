467948734bee70fc3b80d4b91fa3016ccaca4be3
ytimesy <yukio.juna1224@gmail.com>
2023-02-01 09:01:34 +0900

    デプロイエラー対応

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,15 +2,6 @@
 # exit on error
 set -o errexit
 
-# Download chromedriver
-wget https://chromedriver.storage.googleapis.com/109.0.5414.74/chromedriver_linux64.zip
-
-# Unzip ChromeDriver
-unzip chromedriver_linux64.zip
-
-# Make ChromeDriver executable
-chmod +x chromedriver
-
 # Set the path to ChromeDriver in my application
 export CHROME_DRIVER_BIN=`which chromedriver`
 export PATH=$PATH:$CHROME_DRIVER_BIN
