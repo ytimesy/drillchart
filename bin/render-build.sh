37bc361c83e4693ec05d3703b52109a2134ca07f
ytimesy <yukio.juna1224@gmail.com>
2023-02-12 09:46:28 +0900

    デプロイ後エラー対応

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,14 @@
 # exit on error
 set -o errexit
 
+export WD_INSTALL_DIR=`/opt/render/project/src`
+export WD_CHROME_PATH=`/opt/render/project/src`
+export CHROME_DRIVER_BIN=`/opt/render/project/src`
+export PATH=$PATH:$WD_INSTALL_DIR
+export PATH=$PATH:$WD_CHROME_PATH
+export PATH=$PATH:$CHROME_DRIVER_BIN
+
 bundle install
-bundle exec rake webdrivers:chromedriver:update WD_CACHE_TIME=86_400  WD_INSTALL_DIR=/opt/render/project/src WD_CHROME_PATH=/opt/render/project/src
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
